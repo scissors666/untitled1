38dce6dc937e2922ac0761136ff5af00b9dd982f
Scissors <iharkulazhanka@gmail.com>
2016-10-29 04:13:56 +0300

    Add Array to List

--- arrays_task2.js
+++ arrays_task2.js
@@ -1,36 +1,33 @@
 /**
  * Created by Scissors on 29.10.2016.
  */
-var list = {
-    value: null,
-    rest: null
-};
 
 function arrayToList(array) {
-    for (var i = 0; i < array.length; i++) {
-        addToList(array[i]);
-    }
-    return list;
-}
-function addToList(value) {
-    var newNode = {
-        rest: null,
-        value: value
+    var list = {
+        value: null,
+        rest: null
     };
-    if (list.value==null ){
-        list=newNode;
-    }
-    else
-    {
-        lastElement = getLastElement(list);
-        lastElement.rest = newNode;
+  array.forEach(addToList)
+    return list;
+    function addToList(value) {
+        var newNode = {
+            rest: null,
+            value: value
+        };
+        if (list.value === null) {
+            list = newNode;
+        }
+        else {
+            lastElement = getLastElement(list);
+            lastElement.rest = newNode;
+        }
+
     }
 }
-
 function getLastElement(listHeader) {
-    if (listHeader.rest == null) {
+    if (listHeader.rest === null) {
         return listHeader;
     }
     else return getLastElement(listHeader.rest);
 }
-console.log(arrayToList([1, 2, 3]))
+console.log(arrayToList([1, 2, 3]));
